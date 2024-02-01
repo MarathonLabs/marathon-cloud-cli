daba2ea0a6ea9c13f536553dcb2212a4096709e9
Anton Malinskiy <anton@malinskiy.com>
2024-02-01 20:16:58 +1000

    fix(ci): use arg instead of envvar on windows

--- src/bin/mangen.rs
+++ src/bin/mangen.rs
@@ -2,6 +2,7 @@ use clap::CommandFactory;
 use clap_mangen::Man;
 use marathon_cloud::cli::Cli;
 use std::env;
+use std::ffi::OsString;
 use std::fs;
 use std::io::Result;
 
@@ -10,8 +11,13 @@ use std::io::Result;
 /// in a directory specified by the environment variable OUT_DIR.
 /// See <https://doc.rust-lang.org/cargo/reference/environment-variables.html>
 fn main() -> Result<()> {
-    let out_dir =
-        std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
+    let args: Vec<String> = env::args().collect();
+    let first_arg: Option<OsString> = args.get(1).map(|s| s.into());
+    let out_dir = std::env::var_os("OUT_DIR")
+        .or(first_arg)
+        .ok_or(std::io::ErrorKind::NotFound)?;
+    let out_dir = std::path::PathBuf::from(out_dir);
+
     let out_path = out_dir.join(format!("{}.1", env!("CARGO_PKG_NAME")));
     let app = Cli::command();
     let man = Man::new(app);
